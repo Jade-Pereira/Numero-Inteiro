10b7c34f1c26547f9b770f5a5183019002eaa483
Jade Pereira <97922059+Jade-Pereira@users.noreply.github.com>
2023-04-26 08:43:26 -0300

    Add files via upload

---
+++ Numeros inteiros.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    	int n1, n2, soma;
+   printf("Informe um numemro:");
+   scanf("%d",&n1);
+
+   printf("Informe um numero: ");
+   scanf("%d",&n2);
+
+   soma = n1 + n2;
+
+   printf("A soma e: %d \n", soma);
+   
+   return 0;
+
+}
+
